dbf01bd72c150a90993d40eae8a156ba3842200a
Ganin Sergey <mcheguevara2@gmail.com>
2025-04-05 07:05:19 +0300

    remove files

--- cpu-test.js
+++
@@ -1,6 +0,0 @@
-const si = require("systeminformation");
-
-// promises style - new since version 3
-si.cpuTemperature()
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
