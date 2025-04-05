3549481241d10436c9d5a3d10715cf5aaee0f9a3
Ganin Sergey <mcheguevara2@gmail.com>
2025-04-05 06:42:28 +0300

    add resease changes

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const {SerialPort} = require('serialport');
 const {ReadlineParser} = require('@serialport/parser-readline');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 let port = null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
-
   mainWindow.loadFile('index.html');
 }
 
-app.on('ready', createWindow);
+app.on('ready', ()=>{
+  createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
 ipcMain.on('find-arduino', async () => {
   const ports = await SerialPort.list();
@@ -54,4 +57,20 @@ ipcMain.on('send-data', (event, data) => {
   if (port) {
     port.write(data + '\n');
   }
+});
+
+autoUpdater.on('update-available', (info) => {
+  mainWindow.webContents.send('update-available', info);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  mainWindow.webContents.send('update-downloaded', info);
+});
+
+ipcMain.on('download-update', () => {
+  autoUpdater.downloadUpdate();
+});
+
+ipcMain.on('install-update', () => {
+  autoUpdater.quitAndInstall();
 });
